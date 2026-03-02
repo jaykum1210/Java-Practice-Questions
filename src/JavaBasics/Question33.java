fbca9f5d67c36f25e4ae4874776b7e4b4d92dcf4
jaykum1210 <jaykumawat1210@gmail.com>
2026-03-02 10:34:53 +0530

    add question 33 file

---
+++ src/JavaBasics/Question33.java
@@ -0,0 +1,12 @@
+package JavaBasics;
+
+import java.util.Scanner;
+
+public class Question33 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        System.out.println(a/b);
+    }
+}
