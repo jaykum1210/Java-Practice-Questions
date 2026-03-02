02e55587eb315befd9399668aab6ac35b35ab64b
jaykum1210 <jaykumawat1210@gmail.com>
2026-03-02 23:05:58 +0530

    add question 23 file in operator

---
+++ src/Operator/Question23.java
@@ -0,0 +1,18 @@
+package Operator;
+
+import java.util.Scanner;
+
+public class Question23 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter Number : ");
+        int a = sc.nextInt();
+        int sum = 0;
+        while(a>0){
+            int b = a%10;
+            sum+=b;
+            a/=10;
+        }
+        System.out.println("Sum of digits : " + sum);
+    }
+}
