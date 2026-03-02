a86a97146e7bba54719d902a2671fc097b99e44e
jaykum1210 <jaykumawat1210@gmail.com>
2026-03-02 10:26:35 +0530

    update question 29 file

--- src/JavaBasics/Question29.java
+++ src/JavaBasics/Question29.java
@@ -6,6 +6,6 @@ public class Question29 {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
-
+        System.out.println(a);
     }
 }
