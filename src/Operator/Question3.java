d38053c4ea09dc668fe3d76ccc1c89fb59a47653
jaykum1210 <jaykumawat1210@gmail.com>
2026-03-02 19:00:53 +0530

    add question 3 file in operator

---
+++ src/Operator/Question3.java
@@ -0,0 +1,15 @@
+package Operator;
+
+import java.util.Scanner;
+
+public class Question3 {
+    // Multiplication of two Numbers
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter First Number : ");
+        int a = sc.nextInt();
+        System.out.print("Enter Second Number : ");
+        int b = sc.nextInt();
+        System.out.print("Multiply : " + (a*b));
+    }
+}
