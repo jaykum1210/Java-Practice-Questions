c6ffea1f1f3fa32d68bdab49a98f08c8b8c05e54
jaykum1210 <jaykumawat1210@gmail.com>
2026-03-02 19:44:35 +0530

    add question 11 file in operator

---
+++ src/Operator/Question11.java
@@ -0,0 +1,16 @@
+package Operator;
+
+import java.util.Scanner;
+
+public class Question11 {
+    // Min Number
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter First Number : ");
+        int a = sc.nextInt();
+        System.out.print("Enter Second Number : ");;
+        int b = sc.nextInt();
+        boolean c = a<b;
+        System.out.print(c);
+    }
+}
