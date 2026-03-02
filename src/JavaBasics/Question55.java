6ceaac861ddcc4204c1b7fb23134b45d214ab1ff
jaykum1210 <jaykumawat1210@gmail.com>
2026-03-02 11:35:37 +0530

    add question 55 file

---
+++ src/JavaBasics/Question55.java
@@ -0,0 +1,11 @@
+package JavaBasics;
+
+import java.util.Scanner;
+
+public class Question55 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        String a = sc.nextLine();
+        System.out.println(a);
+    }
+}
