9e6e02ab3b2930ae3729b73ab7dc97a2b42629e5
jaykum1210 <jaykumawat1210@gmail.com>
2026-03-02 19:01:49 +0530

    add question 4 file in operator

---
+++ src/Operator/Question4.java
@@ -0,0 +1,15 @@
+package Operator;
+
+import java.util.Scanner;
+
+public class Question4 {
+    //Divide Two Numbers
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter First Number : ");
+        int a = sc.nextInt();
+        System.out.print("Enter Second Number : ");
+        int b = sc.nextInt();
+        System.out.println("Division : " + (a/b));
+    }
+}
