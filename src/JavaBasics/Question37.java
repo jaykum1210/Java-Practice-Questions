ade9b20f741516be07ca1f158bd2fe8cac010175
jaykum1210 <jaykumawat1210@gmail.com>
2026-03-02 10:41:02 +0530

    add question 37 file

---
+++ src/JavaBasics/Question37.java
@@ -0,0 +1,11 @@
+package JavaBasics;
+
+import java.util.Scanner;
+
+public class Question37 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        System.out.println(a*a*a);
+    }
+}
