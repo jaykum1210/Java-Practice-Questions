d0531b30b7914609ebddc197199d35505a694fbb
jaykum1210 <jaykumawat1210@gmail.com>
2026-03-02 11:58:59 +0530

    add question 61 file

---
+++ src/JavaBasics/Question61.java
@@ -0,0 +1,9 @@
+package JavaBasics;
+
+public class Question61 {
+    public static void main(String[] args){
+        int a = Integer.parseInt(args[0]);
+        int b = Integer.parseInt(args[0]);
+        System.out.println(a*b);
+    }
+}
