5c8996a27e010766b3ce8579b64ada4523490887
jaykum1210 <jaykumawat1210@gmail.com>
2026-03-02 10:40:27 +0530

    add question 36 file

---
+++ src/JavaBasics/Question36.java
@@ -0,0 +1,11 @@
+package JavaBasics;
+
+import java.util.Scanner;
+
+public class Question36 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        System.out.println(a*a);
+    }
+}
