74ac04d307bd8eea5f54d55103d03069eb96fe76
jaykum1210 <jaykumawat1210@gmail.com>
2026-03-02 12:15:25 +0530

    add question 64 file

---
+++ src/JavaBasics/Question64.java
@@ -0,0 +1,10 @@
+package JavaBasics;
+
+public class Question64 {
+    public static void main(String[] args){
+        int count = 0;
+        // int i = 0;
+        count = args.length;
+        System.out.print(count);
+    }
+}
