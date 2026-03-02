a9d218c1e1cda7a6930cf279fbbfe487225f0900
jaykum1210 <jaykumawat1210@gmail.com>
2026-03-02 10:28:32 +0530

    update question 29 file

---
+++ src/JavaBasics/Question30.java
@@ -0,0 +1,11 @@
+package JavaBasics;
+
+import java.util.Scanner;
+
+public class Question30 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        double a = sc.nextDouble();
+        System.out.println(a);
+    }
+}
