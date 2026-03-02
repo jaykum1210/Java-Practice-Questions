18f4d1eeed137fa5963175eb37ec885103943e01
jaykum1210 <jaykumawat1210@gmail.com>
2026-03-02 22:49:06 +0530

    add question 20 file in operator

---
+++ src/Operator/Question20.java
@@ -0,0 +1,15 @@
+package Operator;
+
+import java.util.Scanner;
+
+public class Question20 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter First Number : ");
+        int a = sc.nextInt();
+        System.out.print("Enter Second Number : ");
+        int b = sc.nextInt();
+        int c = (a>b)?a:b;
+        System.out.print("Max : " + c);
+    }
+}
