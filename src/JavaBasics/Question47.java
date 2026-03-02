d2095b5adb3d19fd427319c8f045009ff74e9e40
jaykum1210 <jaykumawat1210@gmail.com>
2026-03-02 11:06:31 +0530

    add question 47 file

---
+++ src/JavaBasics/Question47.java
@@ -0,0 +1,11 @@
+package JavaBasics;
+
+import java.util.Scanner;
+
+public class Question47 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        System.out.println(a-=5);
+    }
+}
