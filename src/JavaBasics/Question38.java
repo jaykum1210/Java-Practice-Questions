a11a99ffeb67da92131b36404aed00eb27e5f732
jaykum1210 <jaykumawat1210@gmail.com>
2026-03-02 10:44:11 +0530

    add question 38 file

---
+++ src/JavaBasics/Question38.java
@@ -0,0 +1,17 @@
+package JavaBasics;
+
+import java.util.Scanner;
+
+public class Question38 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter Principal : ");
+        int p = sc.nextInt();
+        System.out.print("Enter Rate : ");
+        int r = sc.nextInt();
+        System.out.print("Enter Time : ");
+        int t = sc.nextInt();
+        float si = (float)(p*r*t)/100;
+        System.out.println("SI : " + si);
+    }
+}
