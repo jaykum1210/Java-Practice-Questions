0a2e2340086742f92da87f71f8eaa342f3a4e1b9
jaykum1210 <jaykumawat1210@gmail.com>
2026-03-02 19:50:22 +0530

    add question 15 file in operator

---
+++ src/Operator/Question15.java
@@ -0,0 +1,13 @@
+package Operator;
+
+import java.util.Scanner;
+
+public class Question15 {
+    // Check Negative
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter First Number : ");
+        int a = sc.nextInt();
+        System.out.print(a<=0);
+    }
+}
