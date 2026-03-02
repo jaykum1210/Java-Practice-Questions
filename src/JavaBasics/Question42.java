fe77934c54083793f738ae9af992625cf9b5806d
jaykum1210 <jaykumawat1210@gmail.com>
2026-03-02 11:00:52 +0530

    add question 42 file

---
+++ src/JavaBasics/Question42.java
@@ -0,0 +1,10 @@
+package JavaBasics;
+
+import javax.swing.text.html.StyleSheet;
+
+public class Question42 {
+    public static void main(String[] args){
+        byte a = 100;
+        System.out.println((int)a);
+    }
+}
