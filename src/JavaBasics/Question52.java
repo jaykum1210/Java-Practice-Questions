c46defc2ecf96340517d11e58ae172c09af05a9a
jaykum1210 <jaykumawat1210@gmail.com>
2026-03-02 11:32:10 +0530

    add question 51 file

---
+++ src/JavaBasics/Question52.java
@@ -0,0 +1,9 @@
+package JavaBasics;
+
+public class Question52 {
+    public static void main(String[] args){
+        int a = 5;
+        double b = 1.55;
+        System.out.println(a+b);
+    }
+}
