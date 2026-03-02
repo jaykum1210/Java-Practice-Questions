46ad228a815bf2740853584499bd676fb9009328
jaykum1210 <jaykumawat1210@gmail.com>
2026-03-02 12:01:31 +0530

    add question 61 file

---
+++ src/JavaBasics/Question62.java
@@ -0,0 +1,10 @@
+package JavaBasics;
+
+public class Question62 {
+    public static void main(String[] args){
+        int a = Integer.parseInt(args[0]);
+        int b = Integer.parseInt(args[1]);
+        int c = Integer.parseInt(args[2]);
+        System.out.print((float)(a+b+c)/3);
+    }
+}
