51c798f7e653cedd6207c2c924a67767c139b8e5
jaykum1210 <jaykumawat1210@gmail.com>
2026-03-02 10:49:07 +0530

    add question 39 file

---
+++ src/JavaBasics/Question39.java
@@ -0,0 +1,12 @@
+package JavaBasics;
+
+import java.util.Scanner;
+
+public class Question39 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        float b = (9.0f/5.0f)*(a) + 32;
+        System.out.println(b);
+    }
+}
