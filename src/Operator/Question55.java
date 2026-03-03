2b3c5759e1e95d7f9e4efffe6e8ee3ec82545f50
jaykum1210 <jaykumawat1210@gmail.com>
2026-03-03 22:08:25 +0530

    add question 55 file in operator

---
+++ src/Operator/Question55.java
@@ -0,0 +1,15 @@
+package Operator;
+
+import java.util.Scanner;
+
+public class Question55 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter First Number : ");
+        int a = sc.nextInt();
+        System.out.println("Enter Second Number : ");
+        int b = sc.nextInt();
+        a*=b;
+        System.out.println("Multiply : " + a);
+    }
+}
