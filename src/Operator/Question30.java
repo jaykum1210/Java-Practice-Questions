1a6246854250bb9b66ca2b9b6d4a47a308a5e105
jaykum1210 <jaykumawat1210@gmail.com>
2026-03-03 13:54:53 +0530

    add question 30 file in operator

---
+++ src/Operator/Question30.java
@@ -0,0 +1,18 @@
+package Operator;
+
+import java.util.Scanner;
+
+public class Question30 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int sum = 0 , i = 0;
+        while(i<5){
+            System.out.println("Enter Marks of " + (i+1) + " subject : ");
+            int marks = sc.nextInt();
+            sum+=marks;
+            i++;
+        }
+        float percentage = (float)sum/5;
+        System.out.println("Percentage : " + percentage);
+    }
+}
