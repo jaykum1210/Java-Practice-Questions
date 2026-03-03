dde24bab127b2a5249dd193de6517fe1c81a081f
jaykum1210 <jaykumawat1210@gmail.com>
2026-03-03 20:40:39 +0530

    add question 48 file in operator

---
+++ src/Operator/Question48.java
@@ -0,0 +1,32 @@
+package Operator;
+
+import java.util.Scanner;
+
+public class Question48 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter First Number : ");
+        int a = sc.nextInt();
+        System.out.println("Enter Second Number : ");
+        int b = sc.nextInt();
+        System.out.println("Enter operator : ");
+        char c = sc.next().charAt(0);
+        switch (c){
+            case '+' :
+                System.out.println(a+b);
+                break;
+            case '-' :
+                System.out.println(a-b);
+                break;
+            case '*' :
+                System.out.println(a*b);
+                break;
+            case '/' :
+                System.out.println(a/b);
+                break;
+            case '%' :
+                System.out.println(a%b);
+                break;
+        }
+    }
+}
