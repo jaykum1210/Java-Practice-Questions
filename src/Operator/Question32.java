a0623f9f8ca62b072492f497317cd0a20d5f8f4d
jaykum1210 <jaykumawat1210@gmail.com>
2026-03-03 13:59:14 +0530

    add question 32 file in operator

--- src/Operator/Question32.java
+++ src/Operator/Question32.java
@@ -2,14 +2,14 @@ package Operator;
 
 import java.util.Scanner;
 
-public class Question31 {
-    //Bitwise And
+public class Question32 {
+    // Bitwise Or
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter First Number : ");
         int a = sc.nextInt();
         System.out.println("Enter Second Number : ");
         int b = sc.nextInt();
-        System.out.println(a&b);
+        System.out.println(a|b);
     }
 }
