cc069c9e6953eb1fbe1e3ba8b22d3bb776c251d8
jaykum1210 <jaykumawat1210@gmail.com>
2026-03-05 23:16:00 +0530

    add question 76 file in operator

---
+++ src/Operator/Question77.java
@@ -0,0 +1,13 @@
+package Operator;
+
+import java.util.Scanner;
+
+public class Question77 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Number : ");
+        int a = sc.nextInt();
+        System.out.println();
+
+    }
+}
