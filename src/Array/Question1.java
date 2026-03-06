ccaa6a195bd68d51ed40dedf14887c6872a17a5e
jaykum1210 <jaykumawat1210@gmail.com>
2026-03-06 22:20:39 +0530

    add question 1 file in Array

---
+++ src/Array/Question1.java
@@ -0,0 +1,21 @@
+package Array;
+
+import java.util.Scanner;
+
+public class Question1 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Size : ");
+        int a = sc.nextInt();
+        int[] arr = new int[a];
+        System.out.println("Enter " + a + " numbers : ");
+        for(int i = 0;i<a;i++){
+            arr[i] = sc.nextInt();
+        }
+        int sum = 0;
+        for (int i = 0;i<a;i++){
+            sum+=arr[i];
+        }
+        System.out.println("Sum : " + sum);
+    }
+}
