4c58a57e35bf4fd433d6a6e65c142490badb67f3
jaykum1210 <jaykumawat1210@gmail.com>
2026-03-06 22:49:01 +0530

    add question 9 file in Array

---
+++ src/Array/Question9.java
@@ -0,0 +1,23 @@
+package Array;
+
+import java.util.Scanner;
+
+public class Question9 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Size : ");
+        int a = sc.nextInt();
+        int[] arr = new int[a];
+        System.out.println("Enter " + a + " Numbers : ");
+        for(int i = 0;i<a;i++){
+            arr[i] = sc.nextInt();
+        }
+        for (int i = 0;i<a-1;i++){
+            if (arr[i]>arr[i+1]){
+                System.out.println("Not Sorted");
+                return;
+            }
+        }
+        System.out.println("Sorted");
+    }
+}
