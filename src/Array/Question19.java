21d15d320a8bbf4fc20b46207802f7e7735f6525
jaykum1210 <jaykumawat1210@gmail.com>
2026-03-10 22:16:51 +0530

    add question 19 file in Array

---
+++ src/Array/Question19.java
@@ -0,0 +1,31 @@
+package Array;
+
+import java.util.Scanner;
+
+public class Question19 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Size of Array 1 : ");
+        int a = sc.nextInt();
+        int[] arr1 = new int[a];
+        System.out.println("Enter "+ a + " Elements : ");
+        for (int i = 0;i<a;i++){
+            arr1[i] = sc.nextInt();
+        }
+        System.out.println("Enter Size of Array 2 : ");
+        int b = sc.nextInt();
+        int[] arr2 = new int[b];
+        System.out.println("Enter "+ b + " Elements : ");
+        for (int i = 0;i<b;i++){
+            arr2[i] = sc.nextInt();
+        }
+        for (int i = 0;i<a;i++){
+            for (int j = 0;j<b;j++){
+                if (arr1[i] == arr2[j]){
+                    System.out.println(arr1[i]);
+                    break;
+                }
+            }
+        }
+    }
+}
