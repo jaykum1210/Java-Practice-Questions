94e0d127de09560d8e58d210e142eb213de5b686
jaykum1210 <jaykumawat1210@gmail.com>
2026-03-10 22:46:10 +0530

    add question 26 file in Array

---
+++ src/Array/Question26.java
@@ -0,0 +1,28 @@
+package Array;
+
+import java.util.Scanner;
+
+public class Question26 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Size : ");
+        int a = sc.nextInt();
+        int[] arr = new int[a];
+        for (int i = 0;i<a;i++){
+            arr[i] = sc.nextInt();
+        }
+        for (int i = 0;i<a-1;i++){
+            for (int j = 0;j<a-i-1;j++){
+                if (arr[j]<arr[j+1]){
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
+            }
+        }
+        System.out.println("Array : ");
+        for (int element : arr){
+            System.out.print(element + " ");
+        }
+    }
+}
