d8d5dd8dc8282e83a2ca17c5c847a919e4c81249
jaykum1210 <jaykumawat1210@gmail.com>
2026-03-13 00:39:01 +0530

    add question 43 file in Array

---
+++ src/Array/Question43.java
@@ -0,0 +1,26 @@
+package Array;
+
+import java.util.Scanner;
+
+public class Question43 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Size : ");
+        int a = sc.nextInt();
+        int[] arr = new int[a];
+        System.out.println("Enter " + a + " Elements of Array 1: ");
+        for (int i = 0;i<a;i++){
+            arr[i] = sc.nextInt();
+        }
+        for (int i = 0;i<a-1;i++){
+            for (int j = 0;j<a-i-1;j++){
+                if (arr[j]<arr[j+1]){
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
+            }
+        }
+        System.out.println(arr[0]*arr[1]);
+    }
+}
