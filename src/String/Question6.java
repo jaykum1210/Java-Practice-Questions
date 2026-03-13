5fb93c2b12a3f2fc069595cffb548ce2911259a5
jaykum1210 <jaykumawat1210@gmail.com>
2026-03-13 08:49:25 +0530

    add question 6 file in String

---
+++ src/String/Question6.java
@@ -0,0 +1,17 @@
+package String;
+
+import java.util.Scanner;
+
+public class Question6 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter String : ");
+        String a = sc.nextLine();
+        String temp = a;
+        a = "";
+        for (int i = 0;i<temp.length();i++){
+            a = a + Character.toLowerCase(temp.charAt(i));
+        }
+        System.out.println("String : " + a);
+    }
+}
