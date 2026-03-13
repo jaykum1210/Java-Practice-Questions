c0f24ec0c0b21c8ae268bc1850a65f432c66c50b
jaykum1210 <jaykumawat1210@gmail.com>
2026-03-13 08:43:29 +0530

    add question 3 file in String

---
+++ src/String/Question3.java
@@ -0,0 +1,19 @@
+package String;
+
+import java.util.Scanner;
+
+public class Question3 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter String : ");
+        String a = sc.nextLine();
+        int count = 0;
+        for (int i = 0;i<a.length();i++){
+            char c = Character.toLowerCase(a.charAt(i));
+            if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u'){
+                count++;
+            }
+        }
+        System.out.println("Number of Vowels : " + count);
+    }
+}
