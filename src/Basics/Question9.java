3aab41a13968d6c0aaacc6f09568b73a964405ad
jaykum1210 <jaykumawat1210@gmail.com>
2026-03-30 21:34:44 +0530

    add Question 9 file in Basics

---
+++ src/Basics/Question9.java
@@ -0,0 +1,12 @@
+package Basics;
+
+import java.util.Scanner;
+
+public class Question9 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Character : ");
+        char a = sc.next().charAt(0);
+        System.out.println("ASCII value of " + a + " = " + (int)a);
+    }
+}
