77f0959efb88d611e91a67db935a4d7814f61bee
jaykum1210 <jaykumawat1210@gmail.com>
2026-03-30 23:38:04 +0530

    add Question 22 file in Basics

---
+++ src/Basics/Question22.java
@@ -0,0 +1,14 @@
+package Basics;
+
+import java.util.Scanner;
+
+public class Question22 {
+    // Multiply Floating Numbers
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Two Numbers : ");
+        float a = sc.nextFloat();
+        float b = sc.nextFloat();
+        System.out.println("Sum : " + (a*b));
+    }
+}
