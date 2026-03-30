807ae2e05d75b949b6854bf5038d99a8f0ff7cd9
jaykum1210 <jaykumawat1210@gmail.com>
2026-03-30 20:45:56 +0530

    add Question 1 file in Basics

---
+++ src/Basics/Question1.java
@@ -0,0 +1,16 @@
+package Basics;
+
+import java.util.Scanner;
+
+public class Question1 {
+    // Sum of 2 Numbers
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Two Numbers : ");
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        System.out.println("Sum : " + (a+b));
+    }
+
+}
