2673e065051c9e7da4bfdc197c2883d0db7bf5b6
jaykum1210 <jaykumawat1210@gmail.com>
2026-03-30 23:15:22 +0530

    add Question 13 file in Basics

---
+++ src/Basics/Question13.java
@@ -0,0 +1,18 @@
+package Basics;
+
+import java.util.Scanner;
+
+public class Question13 {
+    // Vowel or Consonant
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Character : ");
+        char ch = sc.next().charAt(0);
+        if (ch=='a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' || ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U'){
+            System.out.println("Vowel");
+        }
+        else{
+            System.out.println("Consonant");
+        }
+    }
+}
