57ceff9af4a7d2341703dbc522a0ce08f8be2276
jaykum1210 <jaykumawat1210@gmail.com>
2026-03-31 00:39:44 +0530

    add Question 43 file in Basics

---
+++ src/Basics/Question43.java
@@ -0,0 +1,19 @@
+package Basics;
+
+import java.util.Scanner;
+
+public class Question43 {
+    // Reverse a Number
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Number : ");
+        int a = sc.nextInt();
+        int rev = 0;
+        while (a>0){
+            int b = a%10;
+            rev = rev*10 + b;
+            a/=10;
+        }
+        System.out.println("Reverse Number : " + rev);
+    }
+}
