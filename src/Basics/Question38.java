5e18630a6ad7b485c506febb3bdc014fb27e0615
jaykum1210 <jaykumawat1210@gmail.com>
2026-03-31 00:27:17 +0530

    add Question 38 file in Basics

---
+++ src/Basics/Question38.java
@@ -0,0 +1,13 @@
+package Basics;
+
+import java.util.Scanner;
+
+public class Question38 {
+    // Integer to Character
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Number : ");
+        int a = sc.nextInt();
+        System.out.println("Character : " + (char)a);
+    }
+}
