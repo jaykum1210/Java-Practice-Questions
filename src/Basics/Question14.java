58b5fd9f61aeccc2c9b4a30ace7e551d1b7bf5ac
jaykum1210 <jaykumawat1210@gmail.com>
2026-03-30 23:16:26 +0530

    add Question 14 file in Basics

---
+++ src/Basics/Question14.java
@@ -0,0 +1,13 @@
+package Basics;
+
+import java.util.Scanner;
+
+public class Question14 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Number : ");
+        int a = sc.nextInt();
+        System.out.println("Square : " + a*a);
+        System.out.println("Cube : " + a*a*a);
+    }
+}
