75b05c4d0270cbb32853a41c9e0874ca82c06f91
jaykum1210 <jaykumawat1210@gmail.com>
2026-03-30 20:58:35 +0530

    add Question 5 file in Basics

---
+++ src/Basics/Question5.java
@@ -0,0 +1,17 @@
+package Basics;
+
+import java.util.Scanner;
+
+public class Question5 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Number : ");
+        int a = sc.nextInt();
+        if (a%2==0){
+            System.out.println(a + " is even");
+        }
+        else{
+            System.out.println(a + " is odd");
+        }
+    }
+}
