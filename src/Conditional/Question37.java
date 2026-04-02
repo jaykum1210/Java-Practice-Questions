847f8492b7bc08d3bd5e24daaec3fd1a59d3889b
jaykum1210 <jaykumawat1210@gmail.com>
2026-04-02 14:54:27 +0530

    add Question 37 file in Conditional

---
+++ src/Conditional/Question37.java
@@ -0,0 +1,23 @@
+package Conditional;
+
+import java.util.Scanner;
+
+public class Question37 {
+    //Grace Marks
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Marks : ");
+        int a = sc.nextInt();
+        boolean g;
+        if (a<=30 && a>25){
+            System.out.println("Grace : ");
+            g = sc.nextBoolean();
+        }
+        if (a>=33 || g){
+            System.out.println("Pass");
+        }
+        else{
+            System.out.println("Fail");
+        }
+    }
+}
