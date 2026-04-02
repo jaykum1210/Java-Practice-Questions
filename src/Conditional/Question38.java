4ef654b4e05f9431a261fefe651acfd1d7673c45
jaykum1210 <jaykumawat1210@gmail.com>
2026-04-02 15:03:03 +0530

    add Question 38 file in Conditional

---
+++ src/Conditional/Question38.java
@@ -0,0 +1,13 @@
+package Conditional;
+
+import java.util.Scanner;
+
+public class Question38 {
+    //Currency
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Money : ");
+        int a = sc.nextInt();
+        System.out.println("INR : " + a*95);
+    }
+}
