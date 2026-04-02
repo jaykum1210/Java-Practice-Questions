dd3492e4a929412289543c18f8d404b4173a3c36
jaykum1210 <jaykumawat1210@gmail.com>
2026-04-02 21:30:39 +0530

    add Question 50 file in Conditional

---
+++ src/Conditional/Question50.java
@@ -0,0 +1,22 @@
+package Conditional;
+
+import java.util.Scanner;
+
+public class Question50 {
+    //Complex Decision
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Four : ");
+        boolean a = sc.nextBoolean();
+        boolean b = sc.nextBoolean();
+        boolean c = sc.nextBoolean();
+        boolean d = sc.nextBoolean();
+        boolean x = (a&&b)|| (c&&d);
+        if (x){
+            System.out.println("Approved");
+        }
+        else{
+            System.out.println("Declined");
+        }
+    }
+}
