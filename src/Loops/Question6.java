cab8af0fa4c39c7d606ddf801c598beca9a0e0bd
jaykum1210 <jaykumawat1210@gmail.com>
2026-04-02 22:35:42 +0530

    add Question 6 file in Loops

---
+++ src/Loops/Question6.java
@@ -0,0 +1,17 @@
+package Loops;
+
+import java.util.Scanner;
+
+public class Question6 {
+    //Sum of Odd Number
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Number : ");
+        int a = sc.nextInt();
+        int sum = 0;
+        for (int i = 1;i<=a;i+=2){
+            sum+=i;
+        }
+        System.out.println("Sum : " + sum);
+    }
+}
