49d969f0b854260e8aad920343a67bac49f26ea4
jaykum1210 <jaykumawat1210@gmail.com>
2026-04-02 22:45:15 +0530

    add Question 9 file in Loops

---
+++ src/Loops/Question9.java
@@ -0,0 +1,19 @@
+package Loops;
+
+import java.util.Scanner;
+
+public class Question9 {
+    //Sum of digits
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Number : ");
+        int a = sc.nextInt();
+        int sum = 0;
+        while (a>0){
+            int b = a%10;
+            sum+=b;
+            a/=10;
+        }
+        System.out.println("Sum : " + sum);
+    }
+}
