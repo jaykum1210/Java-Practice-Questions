3b84733e5311680342bf97f53c553848d83ee90a
jaykum1210 <jaykumawat1210@gmail.com>
2026-04-05 15:44:48 +0530

    add Question 38 file in Loops

---
+++ src/Loops/Question39.java
@@ -0,0 +1,31 @@
+package Loops;
+
+import java.util.Scanner;
+
+public class Question39 {
+    //Split Sum
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter Number: ");
+        String num = sc.next();
+        int n = num.length();
+        boolean found = false;
+        for (int i = 1; i < n; i++) {
+            int leftSum = 0, rightSum = 0;
+            for (int j = 0; j < i; j++) {
+                leftSum += num.charAt(j) - '0';
+            }
+            for (int j = i; j < n; j++) {
+                rightSum += num.charAt(j) - '0';
+            }
+            if (leftSum == rightSum) {
+                found = true;
+                break;
+            }
+        }
+        if (found)
+            System.out.println("YES, possible split exists");
+        else
+            System.out.println("NO split possible");
+    }
+}
