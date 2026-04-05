c7a81bcab3a4de7b714550be44e58ef5c058a292
jaykum1210 <jaykumawat1210@gmail.com>
2026-04-05 16:04:58 +0530

    add Question 41 file in Loops

--- src/Loops/Question40.java
+++ src/Loops/Question40.java
@@ -3,20 +3,29 @@ package Loops;
 import java.util.Scanner;
 
 public class Question40 {
-    //Self Dividing Number
+    //Split Sum
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter Number : ");
-        int a = sc.nextInt();
-        int temp = a;
-        while (a>0){
-            int b = a%10;
-            if (temp%b!=0){
-                System.out.println("No");
-                return;
+        System.out.print("Enter Number: ");
+        String num = sc.next();
+        int n = num.length();
+        boolean found = false;
+        for (int i = 1; i < n; i++) {
+            int leftSum = 0, rightSum = 0;
+            for (int j = 0; j < i; j++) {
+                leftSum += num.charAt(j) - '0';
             }
-            a/=10;
+            for (int j = i; j < n; j++) {
+                rightSum += num.charAt(j) - '0';
+            }
+            if (leftSum == rightSum) {
+                found = true;
+                break;
+            }
         }
-        System.out.println("Yes");
+        if (found)
+            System.out.println("YES, possible split exists");
+        else
+            System.out.println("NO split possible");
     }
 }
--- src/Loops/Question41.java
+++ src/Loops/Question41.java
@@ -3,17 +3,20 @@ package Loops;
 import java.util.Scanner;
 
 public class Question41 {
-    //Count Ways
+    //Self Dividing Number
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter Number : ");
         int a = sc.nextInt();
-        int count = 2;
-        for (int i = 3;i<=a/2;i+=2){
-            if (a%i==0){
-                count++;
+        int temp = a;
+        while (a>0){
+            int b = a%10;
+            if (temp%b!=0){
+                System.out.println("No");
+                return;
             }
+            a/=10;
         }
-        System.out.println("Number of Ways : " + count);
+        System.out.println("Yes");
     }
 }
