2cb9406d7c032bc804c03d3d681d8ed0c3fe2c4d
jaykum1210 <jaykumawat1210@gmail.com>
2026-04-05 16:02:19 +0530

    add Question 40 file in Loops

---
+++ src/Loops/Question41.java
@@ -0,0 +1,19 @@
+package Loops;
+
+import java.util.Scanner;
+
+public class Question41 {
+    //Count Ways
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Number : ");
+        int a = sc.nextInt();
+        int count = 2;
+        for (int i = 3;i<=a/2;i+=2){
+            if (a%i==0){
+                count++;
+            }
+        }
+        System.out.println("Number of Ways : " + count);
+    }
+}
