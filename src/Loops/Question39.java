59ed393cec8d506906321ceedbbd2547b2fa4662
jaykum1210 <jaykumawat1210@gmail.com>
2026-04-05 16:16:07 +0530

    add Question 42 file in Loops

--- src/Loops/Question39.java
+++ src/Loops/Question39.java
@@ -1,5 +1,33 @@
 package Loops;
 
+import java.util.Scanner;
+
 public class Question39 {
-    //
+    //Sum of Unique Digits
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Number : ");
+        int a = sc.nextInt();
+        int num = 0;
+        int i = 0;
+        int sum = 0;
+        while (a>0){
+            int b = a%10;
+            int temp = num;
+            boolean flag = true;
+            while (temp>0){
+                if (b == temp%10){
+                    flag = false;
+                    break;
+                }
+                temp/=10;
+            }
+            if (flag){
+                num = b*(int)Math.pow(10,i++) + num;
+                sum+=b;
+            }
+            a/=10;
+        }
+        System.out.println("Sum : " + sum);
+    }
 }
