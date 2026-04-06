c45f0e5ad1ed17620bbf5eb1cc234385b3cd00ce
jaykum1210 <jaykumawat1210@gmail.com>
2026-04-06 22:16:22 +0530

    add Question 19 file in Array

---
+++ src/OneDArray/Question19.java
@@ -0,0 +1,37 @@
+package OneDArray;
+
+import java.util.Scanner;
+
+public class Question19 {
+    //Rotate Right
+    public static void reverse(int[] arr, int start, int end){
+        while (start<end){
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+            start++;
+            end--;
+        }
+    }
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Size : ");
+        int a = sc.nextInt();
+        int[] arr = new int[a];
+        System.out.println("Enter " + a + " Elements : ");
+        for (int i = 0;i<a;i++){
+            arr[i] = sc.nextInt();
+        }
+        System.out.println("Enter Position : ");
+        int b = sc.nextInt();
+        b = b%a;
+        reverse(arr,0,a-1);
+        reverse(arr,0,b-1);
+        reverse(arr,b,a-1);
+        System.out.println("Array : ");
+        for (int e : arr){
+            System.out.print(e + " ");
+        }
+    }
+
+}
