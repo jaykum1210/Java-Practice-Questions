ef44b294ce78f24b4a98e16c2340a4888d63f48c
jaykum1210 <jaykumawat1210@gmail.com>
2026-04-06 23:11:07 +0530

    add Question 27 file in Array

---
+++ src/OneDArray/Question27.java
@@ -0,0 +1,24 @@
+package OneDArray;
+
+import java.util.Scanner;
+
+public class Question27 {
+    //Prefix Sum
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Size : ");
+        int a = sc.nextInt();
+        int[] arr = new int[a];
+        System.out.println("Enter " + a + " Elements : ");
+        for (int i = 0;i<a;i++){
+            arr[i] = sc.nextInt();
+        }
+        for (int i = 1;i<a;i++){
+            arr[i] = arr[i-1]+arr[i];
+        }
+        System.out.println("Array : ");
+        for (int e : arr){
+            System.out.print(e + " ");
+        }
+    }
+}
