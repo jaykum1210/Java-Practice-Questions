025e796fbdf1fc834d0d42b608a620d662ada68c
jaykum1210 <jaykumawat1210@gmail.com>
2026-04-07 16:28:23 +0530

    add Question 39 file in Array

---
+++ src/OneDArray/Question39.java
@@ -0,0 +1,38 @@
+package OneDArray;
+
+import java.util.Scanner;
+
+public class Question39 {
+    // Trapping Rain Water
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter size: ");
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        System.out.println("Enter elements:");
+        for (int i = 0; i < n; i++){
+            arr[i] = sc.nextInt();
+        }
+        int left = 0, right = n - 1;
+        int leftMax = 0, rightMax = 0;
+        int water = 0;
+        while (left < right) {
+            if (arr[left] < arr[right]) {
+                if (arr[left] >= leftMax) {
+                    leftMax = arr[left];
+                } else {
+                    water += leftMax - arr[left];
+                }
+                left++;
+            } else {
+                if (arr[right] >= rightMax) {
+                    rightMax = arr[right];
+                } else {
+                    water += rightMax - arr[right];
+                }
+                right--;
+            }
+        }
+        System.out.println("Trapped Water: " + water);
+    }
+}
