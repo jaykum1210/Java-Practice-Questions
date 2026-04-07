9187f46101c9691591d4d2859a7f38dfa33e569d
jaykum1210 <jaykumawat1210@gmail.com>
2026-04-07 14:55:55 +0530

    add Question 35 file in Array

---
+++ src/OneDArray/Question35.java
@@ -0,0 +1,39 @@
+package OneDArray;
+
+import java.util.Scanner;
+
+public class Question35 {
+    //Longest Subarray Sum
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Size : ");
+        int a = sc.nextInt();
+        int[] arr = new int[a];
+        System.out.println("Enter " + a + " Elements : ");
+        for (int i = 0;i<a;i++){
+            arr[i] = sc.nextInt();
+        }
+        System.out.println("Enter Sum : ");
+        int b = sc.nextInt();
+        for (int i = 0;i<a;i++){
+            int sum = arr[i];
+            if (sum==b){
+                System.out.println("Found");
+                return;
+            }
+            else{
+                for (int j = i+1;j<a;j++){
+                    sum+=arr[j];
+                    if (sum==b) {
+                        System.out.println("Found");
+                        return;
+                    }
+                    else if(sum>b){
+                        break;
+                    }
+                }
+            }
+        }
+        System.out.println("Not Found");
+    }
+}
