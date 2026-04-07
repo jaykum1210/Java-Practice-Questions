2277adaeee20fb22dcb29d6a6e17b4a425f07036
jaykum1210 <jaykumawat1210@gmail.com>
2026-04-07 22:14:43 +0530

    add Question 46 file in Array

--- src/OneDArray/Question46.java
+++ src/OneDArray/Question46.java
@@ -1,17 +1,27 @@
 package OneDArray;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Question46 {
     //Chocolate Distribution
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Size : ");
         int a = sc.nextInt();
         int[] arr = new int[a];
         System.out.println("Enter " +a + " Elements : ");
         for (int i = 0;i<a;i++){
             arr[i] = sc.nextInt();
         }
-
+        Arrays.sort(arr);
+        System.out.println("Enter Number : ");
+        int b = sc.nextInt();
+        int min = Integer.MAX_VALUE;
+        for (int i = 0;i<a-b;i++){
+            int diff = arr[i+b-1] - arr[i];
+            if (diff<min) min = diff;
+        }
+        System.out.println("Minimum Difference : " + min);
     }
 }
