ddcb3437c25ff2e521df5730c782a4f57d845f59
jaykum1210 <jaykumawat1210@gmail.com>
2026-04-07 14:54:24 +0530

    add Question 34 file in Array

---
+++ src/OneDArray/Question34.java
@@ -0,0 +1,30 @@
+package OneDArray;
+
+import java.util.Scanner;
+
+public class Question34 {
+    //Sort
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Size : ");
+        int a = sc.nextInt();
+        int[] arr = new int[a];
+        System.out.println("Enter " + a + " Elements : ");
+        for (int i = 0;i<a;i++){
+            arr[i] = sc.nextInt();
+        }
+        for (int i = 0;i<a-1;i++){
+            for (int j = 0;j<a-i-1;j++){
+                if (arr[j]>arr[j+1]){
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
+            }
+        }
+        System.out.println("Array : ");
+        for (int i = 0;i<a;i++){
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
