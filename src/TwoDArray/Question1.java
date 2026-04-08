bface9aaab8c04d41fafc70afb900036024d3786
jaykum1210 <jaykumawat1210@gmail.com>
2026-04-08 16:48:18 +0530

    add Question 1 file in 2DArray

---
+++ src/TwoDArray/Question1.java
@@ -0,0 +1,29 @@
+package TwoDArray;
+
+import java.util.Scanner;
+
+public class Question1 {
+    //Print Matrix
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Row : ");
+        int m = sc.nextInt();
+        System.out.println("Enter Column : ");
+        int n = sc.nextInt();
+        int[][] mat = new int[m][n];
+        for (int i = 0;i<m;i++){
+            for (int j = 0;j<n;j++){
+                System.out.print("Enter Element at row " + (i+1) + " and column " + (j+1) + " : ");
+                mat[i][j] = sc.nextInt();
+            }
+        }
+        System.out.println("Matrix : ");
+        for (int i = 0;i<m;i++){
+            for (int j = 0;j<n;j++){
+                System.out.print(mat[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+}
