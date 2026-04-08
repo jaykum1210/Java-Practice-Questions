9164ffc8571ebb9025ff0e01322a3419170cb73b
jaykum1210 <jaykumawat1210@gmail.com>
2026-04-08 21:10:41 +0530

    add Question 14 file in 2DArray

---
+++ src/TwoDArray/Question14.java
@@ -0,0 +1,28 @@
+package TwoDArray;
+
+import java.util.Scanner;
+
+public class Question14 {
+    // Row Maximum
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Row : ");
+        int m = sc.nextInt();
+        System.out.println("Enter Column : ");
+        int n = sc.nextInt();
+        int[][] mat = new int[m][n];
+        for (int i = 0;i<m;i++){
+            for (int j = 0;j<n;j++){
+                System.out.print("Enter Element at row " + (i+1) + " and column " + (j+1) + " : ");
+                mat[i][j] = sc.nextInt();
+            }
+        }
+        for (int i = 0;i<m;i++){
+            int max = Integer.MIN_VALUE;
+            for (int j = 0;j<n;j++){
+                if (mat[i][j]>max) max = mat[i][j];
+            }
+            System.out.println(max);
+        }
+    }
+}
