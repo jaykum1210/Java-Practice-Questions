271b40dfa7f921349fc4280aa81615466697c6e1
jaykum1210 <jaykumawat1210@gmail.com>
2026-04-18 00:06:52 +0530

    add Question 6 file in JavaMethods

---
+++ src/JavaMethods/Question6.java
@@ -0,0 +1,28 @@
+package JavaMethods;
+
+import java.util.Scanner;
+
+public class Question6 {
+    public int length(String str){
+        int len = 0;
+        while (true){
+            try{
+                str.charAt(len);
+                len++;
+            }
+            catch (Exception e){
+                break;
+            }
+        }
+        return len;
+    }
+    // String Length
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        Question6 q = new Question6();
+        System.out.println("Enter String : ");
+        String str = sc.nextLine();
+        int len = q.length(str);
+        System.out.println("length : " + len);
+    }
+}
