9fc74218bce71d3a37b6a607f773c03389c20291
jaykum1210 <jaykumawat1210@gmail.com>
2026-04-18 00:15:42 +0530

    add Question 7 file in JavaMethods

---
+++ src/JavaMethods/Question7.java
@@ -0,0 +1,23 @@
+package JavaMethods;
+
+import java.util.Scanner;
+
+public class Question7 {
+    public String reverse(String str){
+        String temp = "";
+        int len = str.length();
+        for (int i = len-1;i>=0;i--){
+            temp+=str.charAt(i);
+        }
+        return temp;
+    }
+    //Reverse String
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        Question7 q = new Question7();
+        System.out.println("Enter String : ");
+        String str = sc.nextLine();
+        String rev = q.reverse(str);
+        System.out.println("Reverse : " + rev);
+    }
+}
