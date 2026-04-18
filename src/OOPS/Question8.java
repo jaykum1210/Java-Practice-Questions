723bad38d8685259772e1005350c8348ead50258
jaykum1210 <jaykumawat1210@gmail.com>
2026-04-18 14:01:56 +0530

    add Question 8 file in OOPS

---
+++ src/OOPS/Question8.java
@@ -0,0 +1,11 @@
+package OOPS;
+
+public class Question8 {
+    //Static Method
+    public static void statics(){
+        System.out.println("Hello");
+    }
+    public static void main(String[] args){
+        statics();
+    }
+}
