ffdba1acf03785ad94a433deb5238b450d30cd83
jaykum1210 <jaykumawat1210@gmail.com>
2026-04-22 22:21:12 +0530

    add Question 15 file in OOPS

---
+++ src/OOPS/Question15.java
@@ -0,0 +1,22 @@
+package OOPS;
+
+class Fruit{
+    String name;
+    Fruit(String name){
+        this.name = name;
+    }
+}
+
+public class Question15 {
+    public static void main(String[] args){
+        Fruit[] f = new Fruit[3];
+        f[0] = new Fruit("Apple");
+        f[1] = new Fruit("Orange");
+        f[2] = new Fruit("Banana");
+
+        for (int i = 0;i<3;i++){
+            System.out.println(f[i].name);
+        }
+    }
+
+}
