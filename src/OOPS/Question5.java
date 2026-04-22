10a652944a552e2430c23f1c5d15c1d80dc40c15
jaykum1210 <jaykumawat1210@gmail.com>
2026-04-22 19:55:17 +0530

    add Question 5 file in OOPS

--- src/OOPS/Question5.java
+++ src/OOPS/Question5.java
@@ -1,7 +1,27 @@
 package OOPS;
 
-class Person
+class Person{
+    private int age;
+    public void setAge(int age){
+        if (age>=0){
+            this.age = age;
+        }
+        else{
+            System.out.println("Invalid Age");
+        }
+    }
+    public int getAge(){
+        return age;
+    }
+}
 
 public class Question5 {
-
+    public static void main(String[] args){
+        Person p1 = new Person();
+        Person p2 = new Person();
+        p1.setAge(100);
+        System.out.println("Age : " + p1.getAge());
+        p2.setAge(-5);
+        System.out.println("Age : " + p2.getAge());
+    }
 }
