58d1a81b6272ef05d7642a6f39f44a25e605c92f
jaykum1210 <jaykumawat1210@gmail.com>
2026-04-24 18:07:36 +0530

    add Question 16 file in Class and object

---
+++ src/Class_Object/Question16.java
@@ -0,0 +1,11 @@
+package Class_Object;
+
+class MathConstanta{
+    final static double pi = 3.14159;
+}
+
+public class Question16 {
+    public static void main(String[] args){
+        System.out.println("PI : " + MathConstanta.pi);
+    }
+}
