5d6a83df5fe1b7c686d5d829059e7099c881094a
jaykum1210 <jaykumawat1210@gmail.com>
2026-05-17 15:42:43 +0530

    add Question 1 file in String2

---
+++ src/Strings2/Question1.java
@@ -0,0 +1,12 @@
+package Strings2;
+
+import java.util.Scanner;
+
+public class Question1 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter  : ");
+        String s = sc.nextLine();
+        System.out.println("Hello, " + s);
+    }
+}
